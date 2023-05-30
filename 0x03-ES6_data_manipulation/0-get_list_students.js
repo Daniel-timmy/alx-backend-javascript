f7aa83b38dea87ff43d1eb061ca667dd3bc67095
Daniel-timmy <ajayitimmy45@gmail.com>
2023-05-30 08:48:04 +0100

    task 1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,5 +3,6 @@ export default function getListStudents() {
     const object2 = {id: 2, firstname: "James", location: "Columbia"};
     const object3 = {id: 5, firstname: "Serena", location: "San Francisco"};
     
-    return [object1, object2, object3];
+    const mlist = [object1, object2, object3];
+    return mlist;
 }
