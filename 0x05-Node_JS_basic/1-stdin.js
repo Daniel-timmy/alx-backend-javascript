36dc52982feb48283a96d1a0e48ac776395d709a
Daniel-timmy <ajayitimmy45@gmail.com>
2023-06-16 13:17:14 +0100

    task 1, 5, and 7

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,16 @@
-const readline = require('readline')
-const rInput = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rInput.question('Welcome to Holberton School, what is your name?\n', (rName) => {
-    console.log(`Your name is: ${rName}`);
-    console.log('This important software is now closing');
-    rInput.close();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
